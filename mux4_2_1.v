a2036995b6a0191a4c4579ce36e229db2032e724
Sean Flaherty <sflahert@nano.coe.neu.edu>
2015-12-13 05:08:45 -0500

    asdfadsf

--- mux4_2_1.v
+++ mux4_2_1.v
@@ -8,10 +8,10 @@
  * TODO: Add module documentation
  */
 module mux4_2_1(
-		input [3:0] in0,
-		input [3:0] in1,
+		input [4:0] in0,
+		input [4:0] in1,
 		input s,
-		output reg [3:0] out);
+		output reg [4:0] out);
 	
 	always@(s or in0 or in1)
 		if (s==1) begin
